ce335349ef60cc78fcb282b5607cb4027dcf0def
Ruslan <ruslangazizov36@gmail.com>
2021-10-06 19:18:58 +0300

    Change the config file location

--- app/__init__.py
+++ app/__init__.py
@@ -2,7 +2,7 @@ from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
 from flask_login import LoginManager
 
-from app.configuration import DevelopmentConfig
+from config import DevelopmentConfig
 
 db = SQLAlchemy()
 login_manager = LoginManager()
