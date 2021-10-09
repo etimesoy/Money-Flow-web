8280f82f1c2073960d38c7265877cf30a03411ca
Ruslan <ruslangazizov36@gmail.com>
2021-10-09 09:01:39 +0300

    Add route for settings page and non-implemented settings page itself

--- application/settings.py
+++ application/settings.py
@@ -1,7 +1,14 @@
-from flask import Blueprint
+from flask import Blueprint, render_template
+from flask_login import login_required
 
 settings_bp = Blueprint(
     'settings_bp', __name__,
     template_folder='templates',
     static_folder='static'
 )
+
+
+@settings_bp.route('/settings/')
+@login_required
+def settings():
+    return render_template('settings.html')
