a5e28d1fa969eeee1fe4a909a72fe6ddd001b054
Ruslan <ruslangazizov36@gmail.com>
2021-10-27 14:21:45 +0300

    Update DatabaseManager

--- application/services/database_manager.py
+++ application/services/database_manager.py
@@ -1,41 +1,221 @@
-from datetime import datetime, timedelta
+from datetime import datetime, timedelta, date
+from typing import List
 
+from application import db
+from application.models.category import Category
 from application.models.currency import Currency
 from application.models.transaction import Transaction
+from application.models.user import User
 from application.models.user_categories_limits_asc import UserCategoriesLimitsAsc
+
+
+class LimitAlreadyExistsError(Exception):
+    pass
 
 
 class DatabaseManager:
     @classmethod
-    def get_last_7_days_expenses(cls, user_id: int):
-        return cls.get_last_7_days_transactions(user_id, expenses=True)
+    def get_all_currencies(cls):
+        return list(Currency.query.all())
 
     @classmethod
-    def get_last_7_days_incomes(cls, user_id: int):
-        return cls.get_last_7_days_transactions(user_id, expenses=False)
+    def get_currency_sign(cls, currency_abbreviation: str):
+        with db.engine.connect() as conn:
+            query = """
+            SELECT sign FROM currencies
+            WHERE abbreviation=:currency_abbreviation
+            """
+            result = conn.execute(query, currency_abbreviation=currency_abbreviation).fetchone()
+        return result[0]
 
     @classmethod
-    def get_last_7_days_transactions(cls, user_id: int, expenses: bool = None):
-        min_date = datetime.now().date() - timedelta(days=6)
+    def get_currency_id(cls, currency_abbreviation: str):
+        return Currency.query.filter_by(
+            abbreviation=currency_abbreviation
+        ).first().id
+
+    @classmethod
+    def get_category_id(cls, name: str):
+        return Category.query.filter_by(
+            name=name
+        ).first().id
+
+    @classmethod
+    def get_user(cls, user_id: int = None,
+                 username: str = None,
+                 password: str = None,
+                 full_name: str = None,
+                 email: str = None) -> User:
+        filters = dict(id=user_id, username=username, password=password, full_name=full_name, email=email)
+        filters = {key: value for key, value in filters.items() if value is not None}
+        return User.query.filter_by(
+            **filters
+        ).first()
+
+    @classmethod
+    def get_transactions(cls, user_id: int, expenses: bool = None) -> List[Transaction]:
         filters = {
             'user_id': user_id
         }
         if expenses is not None:
             filters['is_expense'] = expenses
-        return Transaction.query.filter_by(
+        return list(Transaction.query.filter_by(
+            **filters
+        ).all())
+
+    @classmethod
+    def get_days_transactions(cls, user_id: int, offset_weeks_count: int = 0, expenses: bool = None) -> List[Transaction]:
+        max_date = datetime.now().date() + timedelta(days=offset_weeks_count * 7)
+        min_date = max_date - timedelta(days=6)
+        filters = {
+            'user_id': user_id
+        }
+        if expenses is not None:
+            filters['is_expense'] = expenses
+        return list(Transaction.query.filter_by(
             **filters
         ).order_by(
             Transaction.date
         ).where(
-            Transaction.date >= min_date
+            Transaction.date >= min_date,
+            Transaction.date <= max_date
+        ).all())
+
+    @classmethod
+    def get_month_transactions(cls, user_id: int,
+                               year_no: int,
+                               month_no: int,
+                               expenses: bool = None) -> List[Transaction]:
+        filters = {
+            'user_id': user_id,
+        }
+        if expenses is not None:
+            filters['is_expense'] = expenses
+        return list(Transaction.query.filter_by(
+            **filters
+        ).order_by(
+            Transaction.date
+        ).where(
+            Transaction.date.like(f'{year_no}-{month_no}-%')
+        ).all())
+
+    @classmethod
+    def get_limits(cls, user_id: int) -> List[UserCategoriesLimitsAsc]:
+        return list(UserCategoriesLimitsAsc.query.filter_by(
+            user_id=user_id
+        ).order_by(
+            UserCategoriesLimitsAsc.category_id
+        ).all())
+
+    @classmethod
+    def get_limits_by_category_name_part(cls,
+                                         user_id: int,
+                                         category_name: str) -> list:
+        return UserCategoriesLimitsAsc.query.join(
+            Category
+        ).filter(
+            UserCategoriesLimitsAsc.user_id == user_id,
+            Category.name.ilike(f'%{category_name}%')
         ).all()
 
     @classmethod
-    def get_all_currencies(cls):
-        return Currency.query.all()
+    def get_categories_names(cls, user_id: int):
+        user_categories_limits = list(UserCategoriesLimitsAsc.query.filter_by(
+            user_id=user_id
+        ).all())
+        return list(map(lambda x: x.category.name, user_categories_limits))
 
     @classmethod
-    def get_currency_sign(cls, currency_abbreviation: str):
-        return Currency.query.filter_by(
-            abbreviation=currency_abbreviation
-        ).first().sign
+    def update_user(cls, user_id: int, username: str, full_name: str, email: str):
+        user = User.query.filter_by(
+            id=user_id
+        ).first()
+        user.username = username
+        user.full_name = full_name
+        user.email = email
+        db.session.commit()
+
+    @classmethod
+    def add_transaction(cls, user_id: int,
+                        is_expense: bool,
+                        transaction_date: date,
+                        amount: int,
+                        category_name: str,
+                        transaction_name: str,
+                        currency_abbreviation: str):
+        currency_id = cls.get_currency_id(currency_abbreviation)
+        new_transaction = Transaction(
+            user_id=user_id,
+            is_expense=is_expense,
+            date=transaction_date,
+            amount=amount,
+            name=transaction_name,
+            currency_id=currency_id
+        )
+        if len(category_name) > 0:
+            category_id = cls.get_category_id(category_name)
+            new_transaction.category_id = category_id
+        db.session.add(new_transaction)
+        db.session.commit()
+
+    @classmethod
+    def add_category(cls, user_id: int, category_name: str):
+        new_category = Category(
+            name=category_name,
+            image=category_name + '.png'
+        )
+        db.session.add(new_category)
+        db.session.commit()
+        category_id = cls.get_category_id(category_name)
+        new_user_category_limit_asc = UserCategoriesLimitsAsc(
+            user_id=user_id,
+            category_id=category_id
+        )
+        db.session.add(new_user_category_limit_asc)
+        db.session.commit()
+
+    @classmethod
+    def add_limit(cls, user_id: int,
+                  category_name: str,
+                  size: int,
+                  currency_abbreviation: str,
+                  year: int,
+                  month_number: int):
+        category_id = cls.get_category_id(category_name)
+        user_category_limit_asc = UserCategoriesLimitsAsc.query.filter_by(
+            user_id=user_id,
+            category_id=category_id
+        ).first()
+        if user_category_limit_asc.limit_size is not None:
+            raise LimitAlreadyExistsError(f'Limit for category {category_name} already exists')
+
+        currency_id = cls.get_currency_id(currency_abbreviation)
+        user_categories_limits_asc = UserCategoriesLimitsAsc(
+            user_id=user_id,
+            category_id=category_id,
+            limit_size=size,
+            limit_year_number=year,
+            limit_month_number=month_number,
+            currency_id=currency_id
+        )
+        db.session.add(user_categories_limits_asc)
+        db.session.commit()
+
+    @classmethod
+    def update_limit(cls, user_id: int,
+                     category_name: str,
+                     size: int,
+                     currency_abbreviation: str,
+                     year: int,
+                     month_number: int):
+        category_id = cls.get_category_id(category_name)
+        currency_id = cls.get_currency_id(currency_abbreviation)
+        user_categories_limits_asc = UserCategoriesLimitsAsc.query.filter_by(
+            user_id=user_id,
+            category_id=category_id
+        ).first()
+        user_categories_limits_asc.limit_size = size
+        user_categories_limits_asc.limit_month_number = month_number
+        user_categories_limits_asc.limit_year_number = year
+        user_categories_limits_asc.currency_id = currency_id
+        db.session.commit()
