23ba2d0991eb3e4e4ecf70f169e3a6b6f1b636ff
Ruslan <ruslangazizov36@gmail.com>
2021-10-27 10:11:34 +0300

    Add changes into database models

--- application/models/user_categories_limits_asc.py
+++ application/models/user_categories_limits_asc.py
@@ -8,11 +8,13 @@ class UserCategoriesLimitsAsc(UserMixin, db.Model):
 
     __table_args__ = (
         db.PrimaryKeyConstraint("user_id", "category_id"),
+        db.CheckConstraint('limit_size IS NULL OR limit_size >= 0')
     )
 
     user_id = db.Column(db.Integer, db.ForeignKey('users.id'), nullable=False)
     category_id = db.Column(db.Integer, db.ForeignKey('categories.id'), nullable=False)
     limit_size = db.Column(db.Integer)
+    limit_year_number = db.Column(db.Integer)
     limit_month_number = db.Column(db.Integer)
     currency_id = db.Column(db.Integer, db.ForeignKey('currencies.id'))
 
